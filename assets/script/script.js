dbe73b6d765b646f97a759c66b351509cb866839
Pearson <spearson@lwsd.org>
2023-01-08 12:56:50 -0800

    building out javascript

--- assets/script/script.js
+++ assets/script/script.js
@@ -1,56 +1,46 @@
-var button = document.querySelector('#button');
-var temp = document.querySelector('#temp');
+var button = document.querySelector('#add');
+var temp = document.getElementById('#temp');
 var wind = document.querySelector('#wind');
 var humidity = document.querySelector('#humidity');
-var symbol = document.querySelector('#symbol');
-var city = document.querySelector('#city');
+// var symbol = document.querySelector('#symbol');
+var city = document.querySelector('#city').value;
 var descrip = document.querySelector('#descrip');
 
 
-// var getWeather = function (repo) {
-//     var apiUrl = 'api.openweathermap.org/data/2.5/forecast?lat=44.34&lon=10.99&appid=f0d952a79869cf37ed59a5dec2aa36e9';
-//     var apiGeo = 'http://api.openweathermap.org/geo/1.0/direct?q={san diego}&appid=f0d952a79869cf37ed59a5dec2aa36e9'
-// }
+var getWeather = function (repo) {
+    var apiUrl = 'api.openweathermap.org/data/2.5/forecast?lat=44.34&lon=10.99&appid=f0d952a79869cf37ed59a5dec2aa36e9';
+    var apiGeo = 'http://api.openweathermap.org/geo/1.0/direct?q={san diego}&appid=f0d952a79869cf37ed59a5dec2aa36e9'
+}
 
-btn.addEventListener('click', function() {
-    
-            fetch('https://api.openweathermap.org/data/2.5/weather?q='+inputval.value+'&appid='+apik)
+// console.log(city);
+
+button.addEventListener('click', function() {
+    var city = document.querySelector('#city').value;
+        console.log(city)
+            fetch('https://api.openweathermap.org/data/2.5/forecast?q='+city+'&appid=f0d952a79869cf37ed59a5dec2aa36e9&units=imperial')
+            // api.openweathermap.org/data/2.5/forecast?q=+city&appid={API key}
             .then(res => res.json())
-    
             .then(data => {
-    
-                var nameval = data['name']
-                var descrip = data['weather']['0']['description']
-                var temp = data['main']['temp']
-                var wind = data['wind']['speed']
+                for (var i = 0; i < data.length; i++) {
+                    var mainTemp = document.createElement('h4');
+                        mainTemp.textContent = data.list[0].main.temp;
+                        // humidity.textContent = data.list[0].main.humidity;
+                        // wind.textContent = data.list[0].wind.speed;
+                            temp.append(mainTemp);
+                            // humidity.append(humidity);
+                            // wind.append(wind);
+                               
+                                console.log(data)
+                                console.log(data.list[0].main.temp)
+                                console.log(data.list[0].main.humidity)
+                                console.log(data.list[0].wind.speed)
+                    }
 
-                city.innerHTML=`Weather of <span>${nameval}<span>`
-                temp.innerHTML = `Temperature: <span>${ convertion(tempature)} C</span>`
-                description.innerHTML = `Sky Conditions: <span>${descrip}<span>`
-                wind.innerHTML = `Wind Speed: <span>${wndspd} km/h<span>`
     
             })
-    
-            .catch(err => alert('You entered Wrong city name'))
+            
+            // .catch(err => alert('You entered Wrong city name'))
         })
  
-
-
-
-
-    // fetch(apiUrl).then(function (response) {
-    //     if (response.ok) {
-    //       response.json()
-    // };
-
-
-        list.main.temp
-        list.wind.speed
-        list.main.humidity
-        forecast.symbol.var
-        location.name 
-
-    //store datat locally
-    //if statement add style
 
     button.addEventListener('submit', getWeather);
