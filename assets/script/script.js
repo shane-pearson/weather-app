b48fc8e218536df81b651404b88f9867e1839bb9
Pearson <spearson@lwsd.org>
2023-03-08 08:40:05 -0800

    final push

--- assets/script/script.js
+++ assets/script/script.js
@@ -2,9 +2,8 @@ var button = document.querySelector('#add');
 var temp = document.querySelector('#temp');
 var wind = document.querySelector('#wind');
 var humidity = document.querySelector('#humidity');
-var city = document.querySelector('#city').value;
+var city = document.querySelector('#city');
 var descrip = document.querySelector('#descrip');
-
 
 
 var getWeather = function (repo) {
@@ -12,16 +11,34 @@ var getWeather = function (repo) {
     var apiGeo = 'http://api.openweathermap.org/geo/1.0/direct?q={san diego}&appid=f0d952a79869cf37ed59a5dec2aa36e9'
 }
 
-function savedCities () {
-    localStorage.setItem('City', 'data')
-    console.log(localStorage)
-}
+// function getSavedCity() {
+//    var savedCity = localStorage.getItem('city');
+//    if (savedCity) {
+//     var cityContainer = document.querySelector('#savedCity');
+//     var cityElement = document.createElement('p');
+//     cityElement.textContent = savedCity;
+//     cityContainer.appendChild(cityElement);
+//   }
+// }
 
-savedCities()
+function getSavedCity() {
+       var savedCity = localStorage.getItem('city');
+       if (savedCity) {
+        var cityContainer = document.querySelector('#savedCity');
+        var cityButton = document.createElement('button');
+        cityButton.textContent = savedCity;
+        cityButton.classList.add('saved-city-button')
+        cityContainer.appendChild(cityButton);
+        cityContainer.style.display = 'block';
+      }
+    }
 
 function currentWeather() {
     var city = document.querySelector('#city').value;
         console.log(city)
+
+        localStorage.setItem('city', city);
+
             fetch('https://api.openweathermap.org/data/2.5/forecast?q='+city+'&appid=f0d952a79869cf37ed59a5dec2aa36e9&units=imperial')
             // api.openweathermap.org/data/2.5/forecast?q=+city&appid={API key}
             .then(res => res.json())
@@ -29,7 +46,7 @@ function currentWeather() {
                 console.log(data)
                     var html =""
                     {
-                        html += `<div class="card" style="width: 60rem;">
+                        html += `<div class="card" style="width: 65rem;">
  
                     <div class="card-body">
                         <p class="card-text">${data.city.name} ${data.list[0].dt_txt} <img src="https://openweathermap.org/img/wn/${data.list[0].weather[0].icon}@2x.png" class="card-img-top" alt="... "style="max-width:10%;"></p>
@@ -55,9 +72,10 @@ button.addEventListener('click', function() {
                 .then(data => {
                 console.log(data)
                 currentWeather()
+                getSavedCity()
                     var html =""
                     for (var i = 1; i < data.list.length; i=i+8) {
-                        html += `<div class="card" style="width: 10rem;">
+                        html += `<div class="card" style="width: 12rem;">
  
                     <div class="card-body">
                         <p class="card-text"><img src="https://openweathermap.org/img/wn/${data.list[i].weather[0].icon}@2x.png" class="card-img-top" alt="... "style="max-width:50%;"></p>
@@ -73,5 +91,7 @@ button.addEventListener('click', function() {
             // .catch(err => alert('You entered Wrong city name'))
         })
         
+
+    
 
     button.addEventListener('submit', getWeather);
